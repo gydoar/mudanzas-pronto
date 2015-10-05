588a2ccf98b04d0a0733327f65eb4a451fd241f2
Andres Vega <gydoar@hotmail.com>
2015-10-05 10:40:30 -0500

    cambios en los formuarios

--- send2.php
+++ send2.php
@@ -6,13 +6,19 @@
 	// Extract form contents
 	$name = $_POST['nombre'];
 	$tel = $_POST['telefono'];
-	$asunto = $_POST['asunto'];
+	$email = $_POST['email'];
 	$comentarios = $_POST['comentarios'];
+
+	// Validate email address
+	function valid_email($str) {
+		return ( ! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? FALSE : TRUE;
+	}
 		
 	// Return errors if present
 	$errors = "";
 	
 	if($name =='') { $errors .= "nombre,"; }
+	if(valid_email($email)==FALSE) { $errors .= "email,"; }
 	if($comentarios =='') { $errors .= "comentarios,"; }
 
 	// Send email
@@ -22,7 +28,7 @@
 					'Reply-To: '.$email.'' . "\r\n" .
 					'X-Mailer: PHP/' . phpversion();
 		$email_subject = "Nuevo mensaje desde Mudanzas Pronto: $email";
-		$message="Nombre: $name \n\nTel: $tel \n\nAsunto: $asunto \n\nComentarios:\n\n $comentarios";
+		$message="Nombre: $name \n\nTel: $tel \n\nEmail: $email \n\nComentarios:\n\n $comentarios";
 	
 		mail($to, $email_subject, $message, $headers);
 		header('Location: /gracias.html');	
